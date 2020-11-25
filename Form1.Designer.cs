4f3d170fcd346616e1f4d872e79da974458029bb
SeiyaIwabuchi <0123ook@gmail.com>
2020-11-25 23:25:43 +0900

    初回コミット

---
+++ Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace CSV_JSON_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ファイルToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.開くToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.保存ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.終了ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.編集ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.jSONへ変換ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cSVへ変換ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.表示ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.表示クリアToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ヘルプToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.オンラインヘルプToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ファイルToolStripMenuItem,
+            this.編集ToolStripMenuItem,
+            this.表示ToolStripMenuItem,
+            this.ヘルプToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(834, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ファイルToolStripMenuItem
+            // 
+            this.ファイルToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.開くToolStripMenuItem,
+            this.保存ToolStripMenuItem,
+            this.終了ToolStripMenuItem});
+            this.ファイルToolStripMenuItem.Name = "ファイルToolStripMenuItem";
+            this.ファイルToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.ファイルToolStripMenuItem.Text = "ファイル";
+            // 
+            // 開くToolStripMenuItem
+            // 
+            this.開くToolStripMenuItem.Name = "開くToolStripMenuItem";
+            this.開くToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.開くToolStripMenuItem.Text = "開く";
+            // 
+            // 保存ToolStripMenuItem
+            // 
+            this.保存ToolStripMenuItem.Name = "保存ToolStripMenuItem";
+            this.保存ToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.保存ToolStripMenuItem.Text = "保存";
+            // 
+            // 終了ToolStripMenuItem
+            // 
+            this.終了ToolStripMenuItem.Name = "終了ToolStripMenuItem";
+            this.終了ToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.終了ToolStripMenuItem.Text = "終了";
+            // 
+            // 編集ToolStripMenuItem
+            // 
+            this.編集ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.jSONへ変換ToolStripMenuItem,
+            this.cSVへ変換ToolStripMenuItem});
+            this.編集ToolStripMenuItem.Name = "編集ToolStripMenuItem";
+            this.編集ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.編集ToolStripMenuItem.Text = "編集";
+            // 
+            // jSONへ変換ToolStripMenuItem
+            // 
+            this.jSONへ変換ToolStripMenuItem.Name = "jSONへ変換ToolStripMenuItem";
+            this.jSONへ変換ToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.jSONへ変換ToolStripMenuItem.Text = "JSONへ変換";
+            // 
+            // cSVへ変換ToolStripMenuItem
+            // 
+            this.cSVへ変換ToolStripMenuItem.Name = "cSVへ変換ToolStripMenuItem";
+            this.cSVへ変換ToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.cSVへ変換ToolStripMenuItem.Text = "CSVへ変換";
+            // 
+            // 表示ToolStripMenuItem
+            // 
+            this.表示ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.表示クリアToolStripMenuItem});
+            this.表示ToolStripMenuItem.Name = "表示ToolStripMenuItem";
+            this.表示ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.表示ToolStripMenuItem.Text = "表示";
+            // 
+            // 表示クリアToolStripMenuItem
+            // 
+            this.表示クリアToolStripMenuItem.Name = "表示クリアToolStripMenuItem";
+            this.表示クリアToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.表示クリアToolStripMenuItem.Text = "表示クリア";
+            // 
+            // ヘルプToolStripMenuItem
+            // 
+            this.ヘルプToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.オンラインヘルプToolStripMenuItem});
+            this.ヘルプToolStripMenuItem.Name = "ヘルプToolStripMenuItem";
+            this.ヘルプToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.ヘルプToolStripMenuItem.Text = "ヘルプ";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(147, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // オンラインヘルプToolStripMenuItem
+            // 
+            this.オンラインヘルプToolStripMenuItem.Name = "オンラインヘルプToolStripMenuItem";
+            this.オンラインヘルプToolStripMenuItem.Size = new System.Drawing.Size(147, 22);
+            this.オンラインヘルプToolStripMenuItem.Text = "オンラインヘルプ";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 478);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(834, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("游ゴシック", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox1.Location = new System.Drawing.Point(0, 27);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(389, 376);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.WordWrap = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("游ゴシック Medium", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(0, 409);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(389, 66);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "開く";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("游ゴシック Medium", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button2.Location = new System.Drawing.Point(438, 409);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(389, 66);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "保存";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("游ゴシック", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox2.Location = new System.Drawing.Point(438, 27);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(389, 376);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.WordWrap = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("游ゴシック Medium", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(390, 368);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 35);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "→";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(834, 500);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ファイルToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 開くToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 保存ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 終了ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 編集ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem jSONへ変換ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cSVへ変換ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 表示ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 表示クリアToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ヘルプToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem オンラインヘルプToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSV_JSON_Converter
+{
+    public partial class Form1 : Form
+    {
+        private List<List<string>> csvData = new List <List<string>>();
+        private string currentFileName = "";
+        private Encoding currentFileEncoding = Encoding.UTF8;
+        private bool isNowOpening = false;
+        public Form1()
+        {
+            InitializeComponent();
+            button2.Enabled = isNowOpening;
+            statusStrip1.Items.Add(new ToolStripStatusLabel(currentFileEncoding.ToString()));
+            statusStrip1.Items.Add(new ToolStripStatusLabel(currentFileName));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "CSVファイル|*.csv|JSONファイル|*.json";
+            if(openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = "";
+                textBox2.Text = "";
+                csvData = new List<List<string>>();
+                currentFileName = openFileDialog.FileName;
+                using (FileStream fileStream = new FileStream(currentFileName, FileMode.Open, FileAccess.Read))
+                {
+                    StreamReader streamReader = new StreamReader(fileStream,true);
+                    currentFileEncoding = streamReader.CurrentEncoding;
+                    while (!streamReader.EndOfStream)
+                    {
+                        string row = streamReader.ReadLine();
+                        textBox1.Text += row + "\r\n";
+                        string[] rowArray = row.Split(',');
+                        List<string> tmpRowList = new List<string>(rowArray);
+                        csvData.Add(tmpRowList);
+                    }
+                }
+                StringBuilder stringBuilder = new StringBuilder();
+                stringBuilder.Append("[\r\n");
+                for(int index2 = 0;index2 < csvData.Count;index2++)
+                {
+                    stringBuilder.Append("\t[\r\n");
+                    for(int index = 0;index < csvData[index2].Count;index++)
+                    {
+                        if(index < csvData[index2].Count-1) stringBuilder.Append("\t\t\"" + csvData[index2][index] + "\",\r\n");
+                        else stringBuilder.Append("\t\t\"" + csvData[index2][index] + "\"\r\n");
+                    }
+                    if (index2 < csvData.Count - 1) stringBuilder.Append("\t],\r\n");
+                    else stringBuilder.Append("\t]\r\n");
+                }
+                stringBuilder.Append("]\r\n");
+                textBox2.Text = stringBuilder.ToString();
+                isNowOpening = true;
+                button2.Enabled = isNowOpening;
+                statusStrip1.Refresh();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            string saveExtension = "";
+            if (getFileNameExtension() == "csv") {
+                saveFileDialog.Filter = "JSONファイル|*.json";
+                saveExtension = "json";
+            }
+            else if (getFileNameExtension() == "json") {
+                saveFileDialog.Filter = "CSVファイル|*.csv";
+                saveExtension = "csv";
+            }
+            saveFileDialog.FileName = getFileNameWithoutExtension() + saveExtension;
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                using (FileStream fileStream = new FileStream(saveFileDialog.FileName, FileMode.Create, FileAccess.Write))
+                {
+                    StreamWriter streamWriter = new StreamWriter(fileStream);
+                    streamWriter.Write(textBox2.Text);
+                    streamWriter.Flush();
+                }
+            }
+        }
+
+        private string getFileNameExtension()
+        {
+            string[] dodSplited = currentFileName.Split('.');
+            return dodSplited[dodSplited.Length - 1].ToLower();
+        }
+        private string getFileNameWithoutExtension()
+        {
+            string[] dodSplited = currentFileName.Split('.');
+            dodSplited[dodSplited.Length - 1] = "";
+            return String.Join(".", dodSplited);
+        }
+    }
+}
